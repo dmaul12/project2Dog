3d1304cc2beeb772d9a230b244ddbaeb1d1c76d8
David Maul <dmaul12@gmail.com>
2016-06-30 09:02:16 -0400

    start of project

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const express             = require('express');
+const path                = require('path');
+const logger              = require('morgan');
+const bodyParser          = require('body-parser');
+const session             = require('express-session');
+const methodOverride      = require('method-override');
+
+const homeControl         = require('./controller/home_controller');
+const bikesControl        = require('./controller/bikes_controller');
+
+const app                 = express()
+const PORT                = process.env.PORT || process.argv[2] || 3000
+
+app.listen(PORT, function(){
+  console.log("server up and running on port ", PORT)
+});
+
+// set up logging so that we can see what's happening
+app.use( logger('dev') );
+
+// set up ejs to render the views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', homeControl);
+app.use('/search', bikesControl);
+
---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+$(document).ready(function(){
+
+const $body   = $('body');
+const $ul     = $('<ul>');
+const $button = $('.search-bikes')
+
+$button.on('click', function(){
+const titleObj = {};
+
+    if($('.stationName').val()!== ''){
+titleObj['stationName']=$('.status').val()
+
+} if($('.availableBikes').val()!== ''){
+titleObj['availableBikes']=$('.availableBikes').val()
+}
+
+
+$.ajax({
+
+  url:'http://citibikenyc.com/stations/json',
+  method: 'GET',
+  dataType: 'json',
+  data: titleObj,
+  success: function(data) {
+    let $div= $('.list')
+    $ul.empty();
+
+      data.forEach(function(bikes) {
+        let $stationName = $('<li>').text(bikes.stationName);
+        let $availableBikes=$('<li>').text(bikes.availableBikes);
+
+        $ul.append('stationName',$stationName);
+        $ul.append('availableBikes',$availableBikes);
+
+
+
+      })
+      $div.append($ul);
+    }
+})
+})
+})
